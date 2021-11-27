e0be5ba9954b5c58ce37a486272f9f56d925683d
Miguel Santos Correa <miguelsantoscorrea@gmail.com>
2021-11-27 17:10:15 +0100

    add annotations

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,20 @@
 import { useEffect, useState } from "react";
 import "./App.css";
+import { groupBy } from "./groupBy";
 
 interface Annotation {
   annotationId: string;
   frameId: string;
+  x: number;
+  y: number;
+  height: number;
+  width: number;
 }
 
 function App() {
-  const [annotations, setAnnotations] = useState<Annotation[]>();
-  const frameIds = [...new Set(annotations?.map(({ frameId }) => frameId))];
+  const [annotations, setAnnotations] = useState<Annotation[]>([]);
+  const annotationsByFrameId = groupBy(annotations, ({ frameId }) => frameId);
+  const frameIds = [...new Set(annotations.map(({ frameId }) => frameId))];
   useEffect(() => {
     fetch("/data/annotations.json")
       .then((data) => data.json())
@@ -17,12 +23,22 @@ function App() {
   return (
     <div>
       {frameIds.map((frameId) => (
-        <img
-          className="frame-image"
-          src={`/data/images/${frameId}.jpg`}
-          alt="frame"
-          key={frameId}
-        />
+        <div className="frame" key={frameId}>
+          <img
+            className="frame-image"
+            src={`/data/images/${frameId}.jpg`}
+            alt="frame"
+          />
+          {annotationsByFrameId[frameId].map(
+            ({ x, y, height, width, annotationId }) => (
+              <div
+                className="annotation"
+                key={annotationId}
+                style={{ left: x, top: y, height, width }}
+              />
+            )
+          )}
+        </div>
       ))}
     </div>
   );
