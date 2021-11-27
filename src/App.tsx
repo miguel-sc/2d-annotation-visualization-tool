64ffae03c43ca7d2f230543b487f565323aa34b5
Miguel Santos Correa <miguelsantoscorrea@gmail.com>
2021-11-27 17:43:09 +0100

    add frame navigation

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,37 @@
+import { useState } from "react";
 import { Frame } from "./Frame/Frame";
 import { useAnnotations } from "./useAnnotations";
 
 function App() {
   const { frameIds, annotationsByFrameId } = useAnnotations();
+  const [currentFrameIndex, setCurrentFrameIndex] = useState(0);
   return (
     <div>
-      {frameIds.map((frameId) => (
-        <Frame
-          key={frameId}
-          src={`/data/images/${frameId}.jpg`}
-          annotations={annotationsByFrameId[frameId]}
-        />
+      <button
+        onClick={() => setCurrentFrameIndex((index) => index - 1)}
+        disabled={currentFrameIndex === 0}
+      >
+        -
+      </button>
+      <button
+        onClick={() => setCurrentFrameIndex((index) => index + 1)}
+        disabled={currentFrameIndex === frameIds.length - 1}
+      >
+        +
+      </button>
+      {frameIds.map((frameId, frameIndex) => (
+        <div
+          style={{
+            position: "absolute",
+            opacity: frameIndex === currentFrameIndex ? 1 : 0,
+          }}
+        >
+          <Frame
+            key={frameId}
+            src={`/data/images/${frameId}.jpg`}
+            annotations={annotationsByFrameId[frameId]}
+          />
+        </div>
       ))}
     </div>
   );
